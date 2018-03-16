31391031b76719713ba0162a9bfeeaa047ce7180
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-03-16 12:02:42 -0300

    Mejora de estilos

---
+++ src/Components/Containercountry/Containerinput/ContainerMap.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Mapa from './map';
+const Containermap = () =>(
+  <div className='ContainerMap'>
+      <Mapa className = 'Mapa'>
+        
+      </Mapa>
+  </div>
+)
+export default Containermap;
